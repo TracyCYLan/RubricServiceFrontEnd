b78442bd307766bd9c583afd6483e272b180a4ef
TracyCYLan <g0170po@gmail.com>
2020-07-04 22:49:10 +0800

    add scope

--- src/component/RedirectPage.jsx
+++ src/component/RedirectPage.jsx
@@ -12,7 +12,7 @@ class RedirectPage extends Component {
     componentDidMount() {
         // window.location.replace("http://localhost:8080/canvas/oauth_callback?code=324fdjv30tefdvcfew42refd23rfscx")
         // window.location.replace("https://alice.cysun.org/alice-rubrics/canvas/oauth_callback?code=324fdjv30tefdvcfew42refd23rfscx")
-        window.location.replace("https://calstatela.instructure.com:443/login/oauth2/auth?client_id=115900000000000014&response_type=code&scope=url:GET|/api/v1/courses/:course_id/rubrics url:GET|/api/v1/courses url:GET|/api/v1/courses/:course_id/rubrics/:id url:GET|/api/v1/courses/:course_id/outcome_group_links url:GET|/api/v1/outcomes/:id 	url:GET|/api/v1/courses/:course_id/outcome_groups url:POST|/api/v1/courses/:course_id/outcome_groups/:id/outcomes &redirect_uri=https://alice.cysun.org/alice-rubrics/canvas/oauth_callback");
+        window.location.replace("https://calstatela.instructure.com:443/login/oauth2/auth?client_id=115900000000000014&response_type=code&scope=url:GET|/api/v1/courses/:course_id/rubrics url:GET|/api/v1/courses url:GET|/api/v1/courses/:course_id/rubrics/:id url:GET|/api/v1/courses/:course_id/outcome_group_links url:GET|/api/v1/outcomes/:id 	url:GET|/api/v1/courses/:course_id/outcome_groups url:POST|/api/v1/courses/:course_id/outcome_groups/:id/outcomes url:POST|/api/v1/courses/:course_id/rubrics&redirect_uri=https://alice.cysun.org/alice-rubrics/canvas/oauth_callback");
     }
 
     
