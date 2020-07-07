1b198e0b9e0a77d83ee0a5f980d8b0a3cfcdaa6e
TracyCYLan <g0170po@gmail.com>
2020-07-07 14:50:46 +0800

    remove information in the file

--- src/Canvas/component/TestCanvasComponent.jsx
+++ src/Canvas/component/TestCanvasComponent.jsx
@@ -9,7 +9,7 @@ class TestCanvasComponent extends Component {
     }
 
     componentDidMount() {
-        window.sessionStorage.setItem("canvasToken",'11590~VWZMtWiJtlWmE8St8vW8UBmQOoLpX0nhjSUMXZhbPC8eXNE5Pk63FuvNLzVRNYbh');
+        
     }
     
     render() {
