93eb053805174d36a1e633f414e53845654c6572
TracyCYLan <g0170po@gmail.com>
2020-07-23 23:57:21 +0800

    table for grading

--- src/component/assessmentGroup/GetAssessmentGroupComponent.jsx
+++ src/component/assessmentGroup/GetAssessmentGroupComponent.jsx
@@ -43,6 +43,7 @@ class GetAssessmentGroupComponent extends Component {
             })
     }
     countRating() {
+        alert(JSON.stringify(this.state.assessments))
         this.state.criteria.map(
             c => c.ratings.map(r => r['count'] = 0)
         )
@@ -51,6 +52,7 @@ class GetAssessmentGroupComponent extends Component {
                 a.ratings.map((r, index) => {
                     let ratings = this.state.criteria[index].ratings;
                     for (let i in ratings) {
+                        console.log(ratings[i].value)
                         if (ratings[i].value === r.value) {
                             ratings[i].count++;
                             break;
@@ -70,7 +72,6 @@ class GetAssessmentGroupComponent extends Component {
         this.setState({ [e.target.name]: e.target.value });
 
     render() {
-        alert(JSON.stringify(this.state.criteria))
         return (
             [<Breadcrumb key="breadcrumb" className="mx-auto mt-2">
                 <Breadcrumb.Item href="assessmentgroups">AssessmentGroups</Breadcrumb.Item>
@@ -90,25 +91,31 @@ class GetAssessmentGroupComponent extends Component {
                         this.props.history.push('/rubric');
                     }}>Using {this.state.rubric.name}</Card.Text>
                     {
-                        <Table border="1">
+                        <Table bordered responsive="sm" size="sm" style={{ width: 300 }}>
                             <thead>
+                                <tr>
                                 <th>Criterion</th>
                                 <th>Ratings and Counts</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            {
-                                this.state.criteria.map(c =>
-                                    <tr>
-                                        <td>{c.name}</td>
-                                        {
-                                            c.ratings.map(r=>
-                                                [<tr>{r.description}</tr>,
-                                                <td>{r.count}</td>]
-                                                )
-                                        }
-                                    </tr>
-                                )
-                            }
+                                {
+                                    this.state.criteria.map(c =>
+                                        <tr key={c.id}>
+                                            <td>{c.name}</td>
+                                            <td>
+                                                <Table>
+                                                    <tbody>
+                                                    {c.ratings.map((r) =>
+                                                        <tr key={r.id}><td>{r.value} pts</td>
+                                                            <td>{r.count}</td></tr>
+                                                    )}
+                                                    </tbody>
+                                                </Table>
+                                            </td>
+                                        </tr>
+                                    )
+                                }
                             </tbody>
                         </Table>
                     }
