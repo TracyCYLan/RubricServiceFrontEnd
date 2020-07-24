83bcc87713ae477f272be167becae30c41db0373
TracyCYLan <g0170po@gmail.com>
2020-07-24 17:06:24 +0800

    split peer and instructor evaluations

--- src/component/assessmentGroup/GetAssessmentGroupComponent.jsx
+++ src/component/assessmentGroup/GetAssessmentGroupComponent.jsx
@@ -15,7 +15,9 @@ class GetAssessmentGroupComponent extends Component {
             assessDate: '',
             showModal: false,
             rubric: '',
-            criteria: []
+            criteria: [],
+            assessmentPeerCount: 0, //count num of peer assessments
+            assessmentInstructorCount: 0 //count num of instructor assessments
         }
         this.loadAssessmentGroup = this.loadAssessmentGroup.bind(this);
         this.countRating = this.countRating.bind(this);
@@ -43,28 +45,40 @@ class GetAssessmentGroupComponent extends Component {
             })
     }
     countRating() {
-        alert(JSON.stringify(this.state.assessments))
+        // alert(JSON.stringify(this.state.assessments))
         this.state.criteria.map(
-            c => c.ratings.map(r => r['count'] = 0)
+            c => c.ratings.map(r => {
+                r['peer_count'] = 0
+                r['instructor_count'] = 0
+                return r;
+            })
         )
-        this.state.assessments.map(
-            (a) => {
-                a.ratings.map((r, index) => {
-                    let ratings = this.state.criteria[index].ratings;
-                    for (let i in ratings) {
-                        console.log(ratings[i].value)
-                        if (ratings[i].value === r.value) {
-                            ratings[i].count++;
-                            break;
-                        }
+        let peer_count = 0;
+        let ins_count = 0;
+        for (let assessment of this.state.assessments) {
+            if (assessment.type === 'peer_review')
+                peer_count++;
+            else if (assessment.type === 'grading')
+                ins_count++;
+            for (let i = 0; i < assessment.ratings.length; i++) {
+                let r = assessment.ratings[i]; //current rating in this assessment
+                //traverse ratings in certain criterion
+                let ratings = this.state.criteria[i].ratings;
+                for (let rating of ratings) {
+                    if (rating.value === r.value) {
+                        if (assessment.type === 'peer_review')
+                            rating.peer_count++;
+                        else if (assessment.type === 'grading')
+                            rating.instructor_count++;
+                        break;
                     }
-                    return null;
-                })
-                return null;
+                }
             }
-        )
+        }
         this.setState({
-            criteria: this.state.criteria
+            criteria: this.state.criteria,
+            assessmentInstructorCount: ins_count,
+            assessmentPeerCount: peer_count
         })
         console.log(JSON.stringify(this.state.criteria))
     }
@@ -90,34 +104,68 @@ class GetAssessmentGroupComponent extends Component {
                         window.sessionStorage.setItem("rubricId", this.state.rubric.id);
                         this.props.history.push('/rubric');
                     }}>Using {this.state.rubric.name}</Card.Text>
+                    
                     {
-                        <Table bordered responsive="sm" size="sm" style={{ width: 300 }}>
-                            <thead>
-                                <tr>
-                                <th>Criterion</th>
-                                <th>Ratings and Counts</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.state.criteria.map(c =>
-                                        <tr key={c.id}>
-                                            <td>{c.name}</td>
-                                            <td>
-                                                <Table>
-                                                    <tbody>
-                                                    {c.ratings.map((r) =>
-                                                        <tr key={r.id}><td>{r.value} pts</td>
-                                                            <td>{r.count}</td></tr>
-                                                    )}
-                                                    </tbody>
-                                                </Table>
-                                            </td>
-                                        </tr>
-                                    )
-                                }
-                            </tbody>
-                        </Table>
+                        this.state.assessmentInstructorCount === 0 ? "" :
+                            [<Card.Text key="title" className="text-success"> Instructor Evaluations </Card.Text>,
+                            <Table key="table" bordered responsive="sm" size="sm" style={{ width: 300, textAlign: 'center',verticalAlign:'middle'}}>
+                                <thead>
+                                    <tr>
+                                        <th>Criterion</th>
+                                        <th>Ratings and Counts</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.criteria.map(c =>
+                                            <tr key={c.id}>
+                                                <td>{c.name}</td>
+                                                <td>
+                                                    <Table>
+                                                        <tbody>
+                                                            {c.ratings.map((r) =>
+                                                                <tr key={r.id}><td>{r.value} pts</td>
+                                                                    <td>{r.instructor_count}</td></tr>
+                                                            )}
+                                                        </tbody>
+                                                    </Table>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                </tbody>
+                            </Table>]
+                    }
+                    {
+                        this.state.assessmentPeerCount === 0 ? "" :
+                            [<Card.Text key="words" className="text-success"> Peer Evaluations</Card.Text>,
+                            <Table key="table" bordered responsive="sm" size="sm" style={{ width: 300, textAlign: 'center',verticalAlign:'middle'}}>
+                                <thead>
+                                    <tr>
+                                        <th>Criterion</th>
+                                        <th>Ratings and Counts</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.criteria.map(c =>
+                                            <tr key={c.id}>
+                                                <td>{c.name}</td>
+                                                <td>
+                                                    <Table>
+                                                        <tbody>
+                                                            {c.ratings.map((r) =>
+                                                                <tr key={r.id}><td>{r.value} pts</td>
+                                                                    <td>{r.peer_count}</td></tr>
+                                                            )}
+                                                        </tbody>
+                                                    </Table>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                </tbody>
+                            </Table>]
                     }
                 </Card.Body>
             </Card>
