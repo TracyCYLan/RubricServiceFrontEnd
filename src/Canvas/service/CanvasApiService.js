94c5c01f7ad881a741cc643ca6daa1bf259c5ec6
TracyCYLan <g0170po@gmail.com>
2020-08-01 20:22:11 +0800

    remove plural terms in mapping

--- src/Canvas/service/CanvasApiService.js
+++ src/Canvas/service/CanvasApiService.js
@@ -5,22 +5,22 @@ const API_BASE_URL = 'http://localhost:8080/canvas/';
 
 class CanvasApiService {
     fetchCourses(token) {
-        return axios.get(API_BASE_URL + 'courses/token?token=' + token);
+        return axios.get(API_BASE_URL + 'course/token?token=' + token);
     }
     fetchRubrics(courseId, token) {
-        return axios.get(API_BASE_URL + 'courses/' + courseId +
-            '/rubrics/token?token=' + token);
+        return axios.get(API_BASE_URL + 'course/' + courseId +
+            '/rubric/token?token=' + token);
     }
 
     importRubric(courseId, rubricId, token) {
         //get rubric from canvas then post it into db
-        return axios.post(API_BASE_URL + 'courses/' + courseId +
-            '/rubrics/' + rubricId + '/token?token=' + token);
+        return axios.post(API_BASE_URL + 'course/' + courseId +
+            '/rubric/' + rubricId + '/token?token=' + token);
     }
 
     fetchCriteria(courseId, token) {
-        return axios.get(API_BASE_URL + 'courses/' + courseId +
-            '/criteria/token?token=' + token);
+        return axios.get(API_BASE_URL + 'course/' + courseId +
+            '/criterion/token?token=' + token);
     }
 
     importCriterion(criterionId, token) {
@@ -29,13 +29,13 @@ class CanvasApiService {
     }
 
     fetchOutcomeGroups(courseId, token) {
-        return axios.get(API_BASE_URL + 'courses/' + courseId +
-            '/outcome_groups/token?token=' + token);
+        return axios.get(API_BASE_URL + 'course/' + courseId +
+            '/outcome_group/token?token=' + token);
     }
 
     exportCriterion(criterionId, courseId, outcome_group_id, token) {
         return axios.post(API_BASE_URL + 'criterion/' + criterionId +
-            '/export/course/' + courseId + '/outcome_groups/' +
+            '/export/course/' + courseId + '/outcome_group/' +
             outcome_group_id + '/token?token=' + token);
     }
 
@@ -45,13 +45,13 @@ class CanvasApiService {
     }
 
     fetchAssignments(courseId, token) {
-        return axios.get(API_BASE_URL + 'courses/' + courseId +
-            '/assignments/token?token=' + token);
+        return axios.get(API_BASE_URL + 'course/' + courseId +
+            '/assignment/token?token=' + token);
     }
 
     importAssessments(courseId, assignmentId, rubricId, token, assessmentGroupInfo) {
-        return axios.post(API_BASE_URL + 'courses/' + courseId +
-            '/assignments/' + assignmentId + '/rubrics/' + rubricId + '/token?token=' + token, assessmentGroupInfo);
+        return axios.post(API_BASE_URL + 'course/' + courseId +
+            '/assignment/' + assignmentId + '/rubric/' + rubricId + '/token?token=' + token, assessmentGroupInfo);
     }
 }
 
