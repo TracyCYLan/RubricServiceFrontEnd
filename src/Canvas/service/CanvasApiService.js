295d0d7251ab6afe430d19526b1d85317b9271e8
TracyCYLan <g0170po@gmail.com>
2020-09-15 22:28:40 +0800

    add interceptors for axios

--- src/Canvas/service/CanvasApiService.js
+++ src/Canvas/service/CanvasApiService.js
@@ -2,8 +2,42 @@ import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:8080/canvas/';
 const API_BASE_URL = 'https://alice.cysun.org/alice-rubrics/canvas/';
-
+const aliceObj = window.sessionStorage.getItem("oidc.user:https://identity.cysun.org:alice-rubric-service");
+const homepage = '/tlan/#'; // /#/criteria /tlan/#
 class CanvasApiService {
+    constructor() {
+        this.setInterceptors();
+    }
+    setInterceptors() {
+        if (aliceObj) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(aliceObj)['access_token'];
+        }
+        else {
+            axios.defaults.headers.common['Authorization'] = null;
+        }
+        axios.interceptors.response.use(response => {
+            return response;
+        }, error => {
+            if (error.message === 'Network Error' || error.response.status === 502) { //if didn't turn on the spring-boot server 
+                alert('Rubric Server is Down. Please come to visit the site later');
+            }
+            else if (error.response.status === 401) {
+                console.log(error.message)
+                alert("Sorry, your login is expired")
+                window.location.replace(homepage);//go to homepage
+                window.sessionStorage.removeItem("oidc.user:https://identity.cysun.org:alice-rubric-service");
+                window.location.reload(false);
+            }
+            else if (error.response.status === 403) {
+                alert("Sorry, you are not authorized to do this action")
+                window.location.replace(homepage);//go to homepage
+            }
+            else {
+                alert("error status code: " + error.response.status);
+                window.location.replace(homepage);//go to homepage
+            }
+        });
+    }
     fetchCourses(token) {
         return axios.get(API_BASE_URL + 'course/token?token=' + token);
     }
